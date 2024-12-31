73db8c02330f73e32ec19fdb30f0666126bde573
Phil Fuster <pafus23@protonmail.com>
2024-12-31 09:44:52 -0500

    set vi key bindings for fish. add a check for linux environment before running the current config - which was made for linux.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+if status is-interactive; and test (uname) = Linux
+    # Commands to run in interactive sessions on linux
 
     # set up alias for dealing with dotfile git repo
     function config --wraps /usr/bin/git --description 'alias config=/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
@@ -15,6 +15,9 @@ if status is-interactive
         end
     end
 
+    # set user local bin paths
     set -U fish_user_paths ~/.local/bin $fish_user_paths
+    # set vim fish keybindings
+    set -g fish_key_bindings fish_vi_key_bindings
     fish_keychain
 end

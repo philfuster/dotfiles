1869123c3d3d03d4006783f1756b53d98c706bb9
Phil Fuster <pafus23@protonmail.com>
2025-11-02 10:04:49 -0500

    consolidate lsp configs.

--- .config/nvim/lua/plugins/extend-nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/extend-nvim-lspconfig.lua
@@ -1,8 +1,21 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      format_notify = true,
+  "neovim/nvim-lspconfig",
+  opts = {
+    -- Show notifications when formatting (useful for debugging)
+    format_notify = true,
+
+    -- Custom diagnostic display settings
+    diagnostics = {
+      virtual_text = {
+        spacing = 2, -- Tighter spacing than LazyVim default (4)
+        source = false, -- Don't show source in virtual text
+      },
+      float = {
+        border = "rounded",
+        source = true, -- Show source in float window
+        header = "",
+        prefix = "",
+      },
     },
   },
 }

9121ac43b68b900e141f95d4880bb759845f1638
Phil Fuster <paf@storis.com>
2025-11-05 15:10:42 -0500

    update dependencies. format options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,9 +5,9 @@
 -- Require prettier config file for formatting (falls back to biome if not found)
 vim.g.lazyvim_prettier_needs_config = true
 
-vim.opt.updatetime = 200 -- Faster completion and CursorHold
-vim.opt.timeoutlen = 500 -- Faster which-key popup
-vim.opt.scrolloff = 8 -- Keep 8 lines above/below cursor
+vim.opt.updatetime = 200  -- Faster completion and CursorHold
+vim.opt.timeoutlen = 500  -- Faster which-key popup
+vim.opt.scrolloff = 8     -- Keep 8 lines above/below cursor
 vim.opt.sidescrolloff = 8 -- Keep 8 columns left/right of cursor
 
 vim.opt.ignorecase = true
@@ -20,9 +20,9 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 vim.opt.shada = "!,'300,<50,s10,h" -- Smaller shada file
-vim.opt.backup = false -- No backup files
-vim.opt.writebackup = false -- No write backup
-vim.opt.swapfile = false -- No swap (we have undofile)
+vim.opt.backup = false             -- No backup files
+vim.opt.writebackup = false        -- No write backup
+vim.opt.swapfile = false           -- No swap (we have undofile)
 
 -- Set LSP log level to reduce overhead
 vim.lsp.set_log_level("ERROR")
